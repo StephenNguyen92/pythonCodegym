2b16f7b70577b74c8968624f8ae0bfe783bacb49
Stephen <nqhoan92@gmail.com>
2022-02-11 17:26:05 +0700

    changes file

--- HelloWorld/MultiplicationTable.py
+++ HelloWorld/MultiplicationTable.py
@@ -1,4 +1,4 @@
 for i in range(1,10):
     print("Bảng cửu chương của " + str(i))
     for j in range(1,10):
-        print(str(i) + "x" + str(j) + "=" + str(i*j))
+        print("{} x {} = {}".format(i, j, i*j))
